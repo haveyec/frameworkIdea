beabb01a7b2c809f2b42342876bc5f9c7716d9f5
Marlon Henry <haveyec@me.com>
2021-09-11 19:24:13 -0400

    Initial Commit

---
+++ MEgo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MEgo
+//
+//  Created by Marlon Henry on 9/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
